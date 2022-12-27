1c0167f6935221a9a7ab0c285ee4a2077eea0824
BevisDo <doanhtuan52@gmail.com>
2022-12-27 14:06:15 +0700

    #Loading login

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,7 +1,9 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
 import axios from 'axios';
 import { apiUrl, LOCAL_STORAGE_TOKEN_NAME } from './constants';
 import { authReducer } from '../reducers/authReducer';
+import setAuthToken from '../utils/setAuth';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 export const AuthContext = createContext();
 
@@ -11,6 +13,25 @@ const AuthContextProvider = ({ children }) => {
         isAuthenticated: false,
         user: null,
     });
+
+    //Authenticate user
+    const loadUser = async () => {
+        if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
+            setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME]);
+        }
+
+        try {
+            const response = await axios.get(`${apiUrl}/auth`);
+            if (response.data.success) {
+                dispatch({ type: 'SET_AUTH', payload: { isAuthenticated: true, user: response.data.user } });
+            }
+        } catch (error) {
+            localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME);
+            setAuthToken(null);
+            dispatch({ type: 'SET_AUTH', payload: { isAuthenticated: false, user: null } });
+        }
+    };
+    useEffect(()=>loadUser(),[ ])
     //Login
     const loginUser = async (userFrom) => {
         try {
@@ -23,7 +44,7 @@ const AuthContextProvider = ({ children }) => {
         }
     };
     //context data
-    const authContextData = { loginUser };
+    const authContextData = { loginUser, authState };
 
     //return provider
     return <AuthContext.Provider value={authContextData}>{children}</AuthContext.Provider>;
